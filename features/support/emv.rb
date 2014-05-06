59f856032b740f41998d1cba51f46fb7e69a3c65
Robin <robindoble@gmail.com>
2014-05-06 15:45:31 +0100

    bundle installed all gemfiles including capybara and cucumber

---
+++ features/support/emv.rb
@@ -0,0 +1,13 @@
+require_relative '../../myapp'
+ 
+require "Capybara"
+require "Capybara/cucumber"
+require "rspec"
+ 
+ 
+World do
+  Capybara.app = MyApp
+  
+  include Capybara::DSL
+  include RSpec::Matchers
+end
